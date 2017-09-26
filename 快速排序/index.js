85ddbc02b158b6d1a7e92e162a4b1b89e96cf815
huangming1994 <1157114498@qq.com>
2017-09-26 08:49:19 +0800

    注释

--- 快速排序/index.js
+++ 快速排序/index.js
@@ -1,17 +1,17 @@
 function quickSort(arr) {
-  if (!Array.isArray(arr) || arr.length < 2) return arr
-  var pivotIndex = Math.floor(arr.length / 2)
-  var pivot = arr.splice(pivotIndex, 1)[0]
-  var left = []
-  var right = []
+  if (!Array.isArray(arr) || arr.length < 2) return arr  // 递归调用至left或者right项为1项时 直接返回arr
+  var pivotIndex = Math.floor(arr.length / 2)  // 算出基准下标
+  var pivot = arr.splice(pivotIndex, 1)[0]  // 算出基准  并在原数组中删除基准项
+  var left = []  // 存放比基准小的项
+  var right = []  // 存放比基准大的项
   for (var i = 0, len = arr.length; i < len; i++) {
-    if (arr[i] <= pivot) {
-      left.push(arr[i])
+    if (arr[i] < pivot) {
+      left.push(arr[i])  // 比基准小的项
     } else {
-      right.push(arr[i])
+      right.push(arr[i])  // 比基准大的项
     }
   }
-  return quickSort(left).concat([pivot], quickSort(right))
+  return quickSort(left).concat([pivot], quickSort(right)) //  递归调用 并拼接left、基准、right
 }
 
 var arr = [-35, 1, 32, -14, 0, 44, 5, 9, 10, -2]
