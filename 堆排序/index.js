b6241423414a007e541999642ae2208a6eaa1b27
huangming1994 <1157114498@qq.com>
2017-10-15 20:32:08 +0800

    堆排序

--- 堆排序/index.js
+++ 堆排序/index.js
@@ -1,22 +1,22 @@
-function maxHeapify(arr, index, heapSize) {
+function heapify(arr, index, heapSize) {
   var temp, iMax = index, left = 2 * index + 1, right = 2 * index + 2
-  if (left < heapSize && arr[index] < arr[left]) {
+  if (left < heapSize && arr[iMax] < arr[left]) {
     iMax = left
   }
-  if (right < heapSize && arr[index] < arr[right]) {
+  if (right < heapSize && arr[iMax] < arr[right]) {
     iMax = right
   }
   if (iMax !== index) {
     temp = arr[index]
     arr[index] = arr[iMax]
     arr[iMax] = temp
-    maxHeapify(arr, iMax, heapSize)
+    heapify(arr, iMax, heapSize)
   }
 }
 
-function buildMaxHeap(arr, heapSize) {
-  for(var i = Math.floor((heapSize -1) / 2); i >= 0; i--) {
-    maxHeapify(arr, i, heapSize)
+function buildMaxHeap(arr) {
+  for(var i = Math.floor(arr.length / 2) - 1; i >= 0; i--) {
+    heapify(arr, i, arr.length)
   }
 }
 
@@ -27,7 +27,10 @@ function heapSort(arr) {
     var temp = arr[0]
     arr[0] = arr[i]
     arr[i] = temp
-    maxHeapify(arr, 0, i)
+    heapify(arr, 0, i)
   }
   return arr
 }
+
+var arr = [-35, 1, 32, -14, 0, 44, 5, 9, 10, -2]
+heapSort(arr) // [-35, -14, -2, 0, 1, 5, 9, 10, 23, 44]
